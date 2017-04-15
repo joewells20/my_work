7ff9fd4251f67809ac13459e4c0f2987e0c3a199
Joseph Wells <joewells20@hotmail.com>
2017-04-14 23:10:02 -0400

    initial commit

---
+++ dad.rb
@@ -0,0 +1,48 @@
+# puts "can we go to itchy and scratchy land?"
+# x = gets.chomp
+
+
+# until x == "y" do
+# 	 puts "can we go to itchy and scratchy land?"
+# 	x = gets.chomp
+# 	# puts "thanks"
+# end
+
+# def add(y)
+# 	y.each do |i|
+# 		y += i
+# 	end
+# def test
+#     w = []
+#     y = 0
+#     for i in 0...w.length do
+#         y += w[i]
+#     end
+#     assert_equal(10, y)
+# end
+
+# w = []
+# y = gets.chomp
+# until y == "e" do
+# w.<<(y.to_i)
+# y = gets.chomp
+# if y == "e"
+# 	w.each do |x|
+#   sum += x
+# end
+# end
+# 	# puts w.inject(:+)
+# 	# end
+# end
+
+
+
+
+
+# sum = 0
+# [1, 2, 3].each do |i|
+#   sum += i
+# end
+
+# sum = [1, 2, 3].
+# sum.each {|e| sum += e}
